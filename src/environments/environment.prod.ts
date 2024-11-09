0b25de13b086aa127a49e6a562e86dae03c59012
Vinayak vijay kadam <vinayak@Vinayaks-MacBook-Pro.local>
2024-11-09 13:27:03 +0530

    appointment date issue fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     baseUrl: 'https://api.genomicsivfcentre.com/',
-    uploadPath: 'https://api.genomicsivfcentre.com//uploads/',
+    uploadPath: 'https://api.genomicsivfcentre.com/uploads/',
 };
